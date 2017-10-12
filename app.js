5caf2ed185e937ce7255306e0f5ff4dc1ce2a242
aali195 <a.ali15@liverpool.ac.uk>
2017-10-13 01:51:10 +0800

    app.js: Add argument parsing support, case insensitivity and a help command

--- app.js
+++ app.js
@@ -12,15 +12,29 @@ client.on("ready", () => {
 client.on("message", (message) => {
     //Ignore messages not starting with the prefix or if the author is a bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    if (message.content.startsWith(prefix + "ping")) {
-        message.channel.send("pong!");
-    }
-    //Allows changing of the prefix using the command
-    if (message.content.startsWith(prefix + "prefix" || message.author.id == config.ownerID)) {
-        let newPrefix = message.content.split(" ").slice(1,2)[0];
-        config.prefix = newPrefix;
+    //Parse the message by splitting the command and args
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    //Convert to lowercase to achieve case insensitivity
+    const command = args.shift().toLowerCase();
+    
+    //Command list
+    if(command === 'ping') {
+        message.channel.send('Pong!');
+    } else 
+    if (command === 'prefix' && message.author.id == config.ownerID) {
+        config.prefix = args[0];
         //Write to config file
         fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+        message.channel.send('Prefix has been updated and will be used after the bot restarts.');
+    } else
+    if (command === 'help') {
+        const embed = new Discord.RichEmbed()
+        .setTitle("Using the bot")
+        .setColor(0x00AE86)
+        .setDescription("This bot is still a WIP but here are the implemented public commands:")
+        .addField(prefix + "ping", "Print \"pong!\" to the screen.")
+        .addField(prefix + "help", "Print this embeded help message.")
+        message.channel.send({embed});
     }
 });
 
