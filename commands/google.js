c42449f21ff859efbb0e690431594a1d6716b9bd
aali195 <amd5910@gmail.com>
2018-05-05 16:50:32 +0100

    google.js: Implement a Google search command

---
+++ commands/google.js
@@ -0,0 +1,15 @@
+const Discord = require ('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    let embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    // Regex that replaces ',' with a space in args and then replaces the space by a '+'
+    // Args are then passed into the Google search URL
+    .addField('Look it up yourself next time.', '[' + args.toString().replace(/,/g, ' ') + '](https://www.google.com/search?q=' + args.toString().replace(/,/g, '+') + ')')
+    message.channel.send({embed});
+    return;
+}
+
+module.exports.help = {
+    name: "google"
+}
--- commands/help.js
+++ commands/help.js
@@ -10,6 +10,7 @@ module.exports.run = async (client, message, args) => {
     .setDescription('This bot is still a WIP but here are the implemented public commands:')
     .addField(prefix + 'ping', 'Prints \"Pong!\" as well as the latency.')
     .addField(prefix + 'help', 'Prints this embeded help message.')
+    .addField(prefix + 'google', 'Searches a line of text on Google.')
     message.channel.send({embed});
     return;
 }
